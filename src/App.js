74d663c94baf2c4ef56e21c764e4be31e6f4ad9e
jhhest <janvanhest@outlook.com>
2020-01-20 12:10:44 +0100

    added title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      <p>Welcome to React' to 'Welcome to Codaisseur</p>
+      <main>
+        <Title heading="Here comes the title" content="Some text from Jan." />
+      </main>
+      <header className="App-header">
+        <img src={Logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 export default App;
